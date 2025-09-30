7bff438fb46fd06690832eb8e033bacce834ee18
jmartins3 <jmartins@isel.pt>
2025-09-30 17:50:32 +0100

    introduction to cicles

---
+++ lecture-09-30/stats.c
@@ -0,0 +1,52 @@
+/**
+ * Este programa determina se uma sequência de números inteiros positivos está ou não ordenada,
+ * por ordem crescente ou decrescente, ou se todos os elementos são iguais.
+ * A sequência termina quando for introduzido o valor 0.
+ * 
+ * No final, o prgrama deverá emitir uma das seguintes mensagens:
+ * 
+ * "sequência inferior a dois elementos"
+ * "sequência ordenada estritamente por ordem crescente" 
+ * "sequência ordenada estritamente por ordem decrescente"
+ * "sequência com elementos repetidos consecutivos"
+ * "sequência não ordenada"
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int last_n = -1, n;    // valores lidos, anterior e corrente
+    bool equals=false;     // afetado a "true" no caso da ocorrência de elementos repetidos
+    bool increasing=true;  // afetado a "false" caso se verifique impossibilidade de sequência crescente
+    bool decreasing=true;  // afetado a "false" caso se verifique impossibilidade de sequência decrescente
+    int total=0;           // total de valores lidos
+
+    printf("indique uma sequência de inteiros positivos, terminada por 0: ");
+    // ler o primeiro valor
+    scanf("%d", &n);
+
+    while (n != 0) {
+        total++;
+        if (last_n != -1) {
+            if (n <= last_n) {
+                increasing = false;
+            }
+            if (n >= last_n) {
+                decreasing = false;
+            }
+            if (n == last_n) {
+                equals = true;
+            }
+        }
+        last_n = n;
+        // ler novo valor
+        scanf("%d", &n);
+    }
+   
+    // mostrar os resultados
+
+    return 0;
+
+}
+
---
+++ lecture-09-30/tabuada.c
@@ -0,0 +1,24 @@
+/**
+ * Este programa apresenta a tabuada do número dado
+ * (de 1 a 10)
+ */
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("qual a tabuada (1 a 10)? ");
+    scanf("%d", &n);
+
+    if (n < 1 || n > 10 ) {
+        printf("valor inválido!\n");
+        return 1;
+    }
+    int i = 1;
+    while (i <= 10) {
+        // modifique o printf para que a tabuada apareça devidamente tabulada
+        printf("%d x %d = %d\n", n, i, n*i);
+        i = i + 1;
+    }
+    return 0;
+}
