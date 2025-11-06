26dd1877ddbdd1cde2bee633f955ce7b1ffc2f3d
jmartins3 <jmartins@isel.pt>
2025-11-06 10:21:31 +0000

    build a bar histogram

---
+++ lecture-11-06/histogram.c
@@ -0,0 +1,51 @@
+/**
+ * Mostrar a ocorrência de cada letra do alfabeto no texto indicado
+ */
+#include <stdio.h>
+#include <ctype.h>
+
+#define NLETTERS ('Z' - 'A' + 1)
+#define MAX_COLUMN 60
+
+typedef int histogram_t[NLETTERS];
+
+void histo_collect(histogram_t histo) {
+    int c;
+
+    while ((c = getchar()) != -1) {
+        if (isalpha(c)) {
+            // incrementar as ocorrências da respetiva letra
+            c = toupper(c);
+            histo[ c - 'A' ]++;
+        }
+    }
+}
+
+void histo_show1(histogram_t histo) {
+    for(int i=0; i < NLETTERS; ++i) {
+        printf("%c: %4d\n", 'A' + i, histo[i]);     
+    }
+}
+
+void histo_show2(histogram_t histo) {
+    // TODO
+}
+
+
+void histo_show3(histogram_t histo) {
+     // TODO
+}
+
+
+int main() {
+    // declarar e iniciar o array "histo" com zeros
+    histogram_t histo = {0};
+
+    // fase de contabilização
+    histo_collect(histo);
+
+    // fase de apresentação
+    histo_show1(histo);
+    
+    return 0;
+ }
